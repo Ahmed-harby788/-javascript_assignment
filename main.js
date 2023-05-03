f259de659a6fbc77fb50f6bb96707dd3f40618fe
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-03 19:56:28 +0300

    lessons from 01 to 09 -5-

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
-console.group("Group 1");
-console.log("Message one");
-console.log("Message two");
-    console.groupCollapsed("Child Group")
-    console.log("Message one");
-    console.log("Message two");
-        console.groupCollapsed("Grand Child Group")
-        console.log("Message one");
-        console.log("Message two");
-        console.groupEnd()
-    console.groupEnd()
-console.groupEnd()
-console.group("Group 2")
-console.log("Message one");
-console.log("Message two");
+console.table(["Elzero","Ahmed","Sameh","Gamal","Aya"])
