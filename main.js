39067b73ca40694788e9e50d02bacbf2d12fbb4d
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-05 20:10:58 +0300

    lesson from 10 to 17 -2-

--- main.js
+++ main.js
@@ -1,9 +1,2 @@
-let numberOne = 10;
-let numbertwo = 20;
-console.log(numberOne + "" + numbertwo);
-console.log(typeof (numberOne + "" + numbertwo));
-console.log(`${numberOne}${numbertwo}`);
-console.log(typeof `${numberOne}${numbertwo}`);
-console.log(numberOne + "\n" + numbertwo);
-console.log(`${numberOne} 
-${numbertwo}`);
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
