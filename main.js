4cd8ee1582dabc019c811784f38815ddcb1102c2
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-05 20:55:19 +0300

    lesson from 10 to 17 -3-

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("\`I\'m In\n\\\\\nLove \\\\ \"\"\"\'\'\'\n++ with ++\n\\ \"\"\"\\ \"\"\"\n\"\"javascript\"\"\`\`")
+let a = 21;
+let b = 20;
+let c = "_" + b + "" + a ;
+let rep = c.repeat(3)
+
+console.log(`_${a}${rep}_${b}_`); // _21_2021_2021_2021_20_
