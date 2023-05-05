66350984536bf81388a1fab1e461986677140b12
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-05 19:46:00 +0300

    lesson from 10 to 17 -1 -

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.table(["Elzero","Ahmed","Sameh","Gamal","Aya"])
+let numberOne = 10;
+let numbertwo = 20;
+console.log(numberOne + "" + numbertwo);
+console.log(typeof (numberOne + "" + numbertwo));
+console.log(`${numberOne}${numbertwo}`);
+console.log(typeof `${numberOne}${numbertwo}`);
+console.log(numberOne + "\n" + numbertwo);
+console.log(`${numberOne} 
+${numbertwo}`);
