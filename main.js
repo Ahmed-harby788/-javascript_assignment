6c03654ed37350de64715cfb3e68af7276c896bd
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-08 15:32:42 +0300

    lesson from 18 to 22 -all Assignment-

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
-let a = 21;
-let b = 20;
-let c = "_" + b + "" + a ;
-let rep = c.repeat(3)
+// Replace ? With Arithmetic Operators
+console.log((10 * 20) + (15 % 3) + (190 + 10) - 400); // 0
 
-console.log(`_${a}${rep}_${b}_`); // _21_2021_2021_2021_20_
+//.....................................................................
+
+let num = 3;
+
+// Solution One
+console.log(num * num -num); // 6
+
+ // Solution Two
+console.log(num + num); // 6
+
+// Soultion Three
+console.log(num + true + true + true); // 6
+
+// Solution Five
+console.log(num * num - true - true -true ); // 6
+
+// Soultion Four
+console.log(++num + true + true); // 6
+
+// Solution Six
+console.log(--num + num ); // 6
+
+//....................................................................
+
+let n1um = "10";
+
+// Solution One
+console.log(+n1um + +n1um); // 20
+
+// Solution Two
+console.log(+n1um * true + +n1um); // 20
+
+// Solution Three
+console.log(++n1um + +n1um -true -true); // 20
+
+// Solution Four
+console.log(--n1um + +n1um); // 20
+
+//....................................................................
+
+let points = 10;
+
+// Write Your Code Here
+
+console.log(points + true + true + true); // 13
+
+// Write Your Code Here
+
+console.log(points - true - true); // 8;
