4e5caded495ea7c986f245cc901ad807b76d550a
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-13 16:21:12 +0300

    lesson from 23 to 26-1-

--- main.js
+++ main.js
@@ -1,52 +1,16 @@
-// Replace ? With Arithmetic Operators
-console.log((10 * 20) + (15 % 3) + (190 + 10) - 400); // 0
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
 
-//.....................................................................
-
-let num = 3;
-
-// Solution One
-console.log(num * num -num); // 6
-
- // Solution Two
-console.log(num + num); // 6
-
-// Soultion Three
-console.log(num + true + true + true); // 6
-
-// Solution Five
-console.log(num * num - true - true -true ); // 6
-
-// Soultion Four
-console.log(++num + true + true); // 6
-
-// Solution Six
-console.log(--num + num ); // 6
-
-//....................................................................
-
-let n1um = "10";
-
-// Solution One
-console.log(+n1um + +n1um); // 20
-
-// Solution Two
-console.log(+n1um * true + +n1um); // 20
-
-// Solution Three
-console.log(++n1um + +n1um -true -true); // 20
-
-// Solution Four
-console.log(--n1um + +n1um); // 20
-
-//....................................................................
-
-let points = 10;
-
-// Write Your Code Here
-
-console.log(points + true + true + true); // 13
-
-// Write Your Code Here
-
-console.log(points - true - true); // 8;
+// Your Solutions
+console.log(1e5); // 100000
+console.log(Math.pow(10 , 5)); // 100000
+console.log(Math.max(100000)); // 100000
+console.log(1e5 * 1); // 100000
+console.log(2e4 + 8e4); // 100000
+console.log(10 ** 5 ); // 100000
+console.log(2e5 - 1e5); // 100000
+console.log(1e6 / 10); // 100000
+console.log(100_000^0); // 100000 
+console.log(100000.0); // 100000
