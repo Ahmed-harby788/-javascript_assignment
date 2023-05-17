ffd75bc674701e1421caeaa799833cf768a6867b
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-17 19:02:33 +0300

    lesson from 23 to 26-5-

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-//assignments 2
-console.log(Number.MIN_SAFE_INTEGER * -1); // 9007199254740991
-//assignments 3
-console.log(Number.MAX_SAFE_INTEGER + Number.MIN_SAFE_INTEGER + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true + 
-    true +
-    true +
-    true +
-    true)
-//assignments 4
-let myVar = "100.56789 Views";
-console.log(Number(myVar.substring(0,3))); // 100
-console.log((Number(myVar.substring(0,5))) + 0.07); // 100.57
+let num = 10;
+
+console.log(Number.isInteger(num) + true); // 2
