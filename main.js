e5987526f14ae1c2f3a61fd567798bea3bd020a6
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-25 22:35:44 +0300

    lesson from 27 to 30-2-

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-let userName = "Elzero";
-console.log(userName [0].toLowerCase()); // e
-console.log(userName.charAt(0).toLowerCase()); // e
-console.log(userName.substring(0, 1).toLowerCase()); // e
-console.log(userName.substr(0, 1).toLowerCase()); // e
-console.log(userName.at(0).toLowerCase()); // e
-console.log(userName.substring(-userName.length,1).toLowerCase().repeat(3)); // eee
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+console.log(word.includes(letterZ)); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
