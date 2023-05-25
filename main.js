ab037588b21dada062aa3be1560b9ffa5c0b7161
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-25 22:28:19 +0300

    lesson from 27 to 30-1-

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log(Math.floor(Math.random() * 4)); // 0 || 1 || 2 || 3 || 4
+let userName = "Elzero";
+console.log(userName [0].toLowerCase()); // e
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.substr(0, 1).toLowerCase()); // e
+console.log(userName.at(0).toLowerCase()); // e
+console.log(userName.substring(-userName.length,1).toLowerCase().repeat(3)); // eee
