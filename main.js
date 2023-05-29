f9d96a931e9677b8a37806a58d1c92549b725b52
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-29 13:59:38 +0300

    lesson from 40 to 47 correct-1-

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let myFriends = ["Ahmed", "Elham", "Osama", "Gamal"];
 let num = 3;
 
 // Method 1
-console.log(myFriends.splice(0, 3)); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.splice(0, num)); // ["Ahmed", "Elham", "Osama"];
 
 // Method 2
-console.log(myFriends.slice(0, 3)); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.slice(0, num)); // ["Ahmed", "Elham", "Osama"];
