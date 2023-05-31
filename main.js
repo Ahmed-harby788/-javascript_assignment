f1a8b0f17b61d4795e6b88b401fd4fcdad954281
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-31 20:42:05 +0300

    lessons from 63 to 57 - 1 -

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
-let index = 0;
-let counter = 0;
-
-while(index < friends.length){
-    if(typeof friends[index] === "number" || friends[index].startsWith("A")){
-        index++;
-        continue;
+function sayHello(theName, theGender = "") {
+    if (theGender === "Male"){
+        theGender = `Mr`;
     }
-    console.log(`${++counter} => ${friends[index]}`)
-    index++;
-
+    else if( theGender === "Female"){
+        theGender = `Miss`;
+    }
+document.write(`<p> "Hello ${theGender} ${theName}" </p>`);
 }
+  // Needed Output
 
-
-// Output
-"1 => Sayed"
-"2 => Mahmoud"
+    sayHello("Osama", "Male"); // "Hello Mr Osama"
+    sayHello("Eman", "Female"); // "Hello Miss Eman"
+    sayHello("Sameh"); // "Hello Sameh"
