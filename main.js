d2f7ae689506ca6b016bb164f73fc52ee7da0c57
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-05-31 21:05:17 +0300

    lessons from 63 to 57 - 2 -

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
-function sayHello(theName, theGender = "") {
-    if (theGender === "Male"){
-        theGender = `Mr`;
-    }
-    else if( theGender === "Female"){
-        theGender = `Miss`;
-    }
-document.write(`<p> "Hello ${theGender} ${theName}" </p>`);
-}
-  // Needed Output
+let result = 0;
 
-    sayHello("Osama", "Male"); // "Hello Mr Osama"
-    sayHello("Eman", "Female"); // "Hello Miss Eman"
-    sayHello("Sameh"); // "Hello Sameh"
+function calculate(firstNum, secondNum = " Second Number Not Found", operation) {
+    if(operation === 'add'){
+        result = firstNum + secondNum ;
+    }
+    else if (operation === 'subtract'){
+        result = firstNum - secondNum ;
+    }
+    else if (operation === 'multiply'){
+        result = firstNum * secondNum ;
+    }
+    else{
+        result = firstNum + secondNum ;
+    }
+    console.log(result)
+}
+
+  // Needed Output
+  calculate(20); // Second Number Not Found
+  calculate(20, 30); // 50
+  calculate(20, 30, 'add'); // 50
+  calculate(20, 30, 'subtract'); // -10
+  calculate(20, 30, 'multiply'); // 600
