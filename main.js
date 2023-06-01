961435084373e2004e15cc1fabd8f9d3b88ec859
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-01 12:42:07 +0300

    lessons from 63 to 57 - 4 - challenge

--- main.js
+++ main.js
@@ -1,18 +1,47 @@
-let result = 0;
-function ageInTime(theAge) {
-    if(10 > theAge || theAge > 100 ){
-        console.log("Age Out Of Range");
+function checkStatus(a, b, c) {
+    let name;
+    let age;
+    let status;
+    if(typeof a === "string"){
+        name = a
+    }else if(typeof a === "number"){
+        age = a;
+    }else{
+        if(a === true){
+            status = "You Are Available For Hire"
+        }else{
+            status = "You Are Not Available For Hire"
+        }
     }
-    else{
-        console.log(`Age is ${theAge} years`)
-        console.log(`Age is ${theAge * 12} Months`)
-        console.log(`Age is ${theAge * 12 * 30} days`)
-        console.log(`Age is ${theAge * 12 * 30 * 24} hours`)
-        console.log(`Age is ${theAge * 12 * 30 * 24 * 60} minute`)
-        console.log(`Age is ${theAge * 12 * 30 * 24 * 60 * 60} second`)
+    if(typeof b === "string"){
+        name = b
+    }else if(typeof b === "number"){
+        age = b;
+    }else{
+        if(b === true){
+            status = "You Are Available For Hire"
+        }else{
+            status = "You Are Not Available For Hire"
+        } 
     }
+    if(typeof c === "string"){
+        name = c
+    }else if(typeof c === "number"){
+        age = c;
+    }else{
+        if(c === true){
+            status = "You Are Available For Hire"
+        }else{
+            status = "You Are Not Available For Hire"
+        } 
+    }
+    document.write("<div>")
+    document.write(`<p> Hello ${name}, Your Age Is ${age}, ${status} <p>`)
+    document.write("</div>")
 }
 
   // Needed Output
-  ageInTime(110); // Age Out Of Range
-  ageInTime(38); // Months Example => 456 Months
+  checkStatus("Osama", 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+  checkStatus(38, "Osama", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+  checkStatus(true, 38, "Osama"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+  checkStatus(false, "Osama", 38); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
