cbed7f28808a10d63158b58be59eb48f36f3bdb6
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-01 16:56:24 +0300

    lessons from 63 to 57 - 6 -

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-let result;
-function createSelectBox(startYear, endYear) {
-    document.write(`<select>`)
-    for(let i = startYear; i <= endYear; i++){
-        document.write(`<option value = ${i}> ${i} </option>`)
+let result = 0;
+
+function multiply (...number){
+    for(i = 0; i < number.length; i++ ){
+        if(typeof number[i] === "string"){
+            continue;
+        }else{
+            result = parseInt(number[i - 1]) * parseInt(number[i])
+        }
     }
-    document.write(`</select>`)
+    return result; 
 }
-createSelectBox(2000, 2021);
+
+console.log(multiply(10, 20)); // 200
+console.log(multiply("A", 10, 30)); // 300
+console.log(multiply(100.5, 10, "B")); // 1000
