e7918ac6953e445f1adea9182d3b028cd6562f7c
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-03 18:49:21 +0300

    lessons from 71 to 78 - 1 -

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
+let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
 
-function specialMix(...data) {
-  let result = 0;
-  for (let i = 0; i < data.length; i++)
-    if (parseInt(data[i])) result += parseInt(data[i]);
-  if (result === 0) return "All Is Strings";
-  return result;
-}
-
-console.log(specialMix(10, 20, 30)); // 60
-console.log(specialMix("10Test", "Testing", "20Cool")); // 30
-console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
-console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
+let letter = mix.map(function (ele){
+  return isNaN(parseInt(ele)) ? ele: ""})
+  .reduce(function (acc, current){
+    return `${acc}${current}`
+  })
+  console.log(letter)
+// Elzero
