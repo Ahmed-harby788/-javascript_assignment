a8bbb0923d656524cf76a82e484312fa2e01b6c9
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-03 14:13:59 +0300

    lessons from 64 to 70 - 2 -

--- main.js
+++ main.js
@@ -1,26 +1,18 @@
-function getDetails(zName, zAge, zCountry) {
-    function namePattern(zName) {
-        return `${zName.slice(0, zName.indexOf(" "))}${zName.substr(zName.indexOf(" "), 2).toUpperCase()}`
-    }
+// function itsMe() {
+//   return `Iam A Normal Function`;
+// }
 
-    function ageWithMessage(zAge) {
-        return `Your Age Is ${parseInt(zAge)}`
-      // 38 Is My Age => Your Age Is 38
-      // 32 Is The Age => Your Age Is 32
-    }
-    function countryTwoLetters(zCountry) {
-            return `You Live In ${zCountry.substr(0, 2)}`
-      // Egypt => You Live In EG
-      // Syria => You Live In SY
-    }
-    function fullDetails() {
-        return `Hello ${namePattern(zName)},${ageWithMessage(zAge)},${countryTwoLetters(zCountry)}`;
-    }
-    return fullDetails(); // Do Not Edit This
-}
+let itsMe = () => `Iam A Normal Function`;
 
-console.log(getDetails("Osama Mohamed", "38 Is My Age", "Egypt"));
-  // Hello Osama M., Your Age Is 38, You Live In EG
 
-console.log(getDetails("Ahmed ali", "32 Is The Age", "Syria"));
-// Hello Ahmed A., Your Age Is 32, You Live In SY
+console.log(itsMe()); // Iam A Normal Function
+
+
+// function urlCreate(protocol, web, tld) {
+//   return `${protocol}://www.${web}.${tld}`;
+// }
+
+let urlCreate = (protocol, web, tld) => `${protocol}://www.${web}.${tld}`;
+
+
+console.log(urlCreate("https", "elzero", "org")); // https://www.elzero.org
