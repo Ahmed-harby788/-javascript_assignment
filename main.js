f7a6f5327a1c70925b680668aeb7ded951bba5be
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-03 16:34:36 +0300

    lessons from 64 to 70 - 4 -

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-// function checker(zName) {
-//   return function (status) {
-//     return function (salary) {
-//       return status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
-//     };
-//   };
-// }
 
-let checker = (zName) => (status) => (salary) => status === "Available" ? `${zName}, My Salary Is ${salary}` : `Iam Not Avaialble`;
+function specialMix(...data) {
+  let result = 0;
+  for (let i = 0; i < data.length; i++)
+    if (parseInt(data[i])) result += parseInt(data[i]);
+  if (result === 0) return "All Is Strings";
+  return result;
+}
 
-
-
-
-
-console.log(checker("Osama")("Available")(4000)); // Osama, My Salary Is 4000
-console.log(checker("Ahmed")("Not Available")()); // Iam Not Avaialble
+console.log(specialMix(10, 20, 30)); // 60
+console.log(specialMix("10Test", "Testing", "20Cool")); // 30
+console.log(specialMix("Testing", "10Testing", "40Cool")); // 50
+console.log(specialMix("Test", "Cool", "Test")); // All Is Strings
