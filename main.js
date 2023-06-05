b24c6fdaef2ae14bfc43078386e7ab2f1efc430b
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-05 15:26:35 +0300

    lessons from 79 to 85 - 4 -

--- main.js
+++ main.js
@@ -1,26 +1,67 @@
-let a = 1;
-
-let threeNums = {
-b: 2,
-c: 3,
-d: 4,
+// The Object To Work With
+let myFavGames = {
+    "Trinity Universe": {
+    publisher: "NIS America",
+    price: 40,
+    },
+    "Titan Quest": {
+    publisher: "THQ",
+    bestThree: {
+        one: "Immortal Throne",
+        two: "Ragnarök",
+        three: "Atlantis",
+    },
+    price: 50,
+    },
+    YS: {
+    publisher: "Falcom",
+    bestThree: {
+        one: "Oath in Felghana",
+        two: "Ark Of Napishtim",
+        three: "origin",
+    },
+    price: 40,    },
 };
 
-let twoNums = {
-e: 5,
-f: 6,
-};
+  // Code One => How To Get Object Length ?
+let objectLength = Object.keys(myFavGames).length
 
-// Create Your Object Here in One Line
-let finalObject = Object.assign({a}, threeNums, twoNums)
+let gameName = Object.keys(myFavGames)
 
-console.log(finalObject);
+for (let i = 0; i < objectLength; i++) {
+    console.log(`The Game Name Is ${gameName[i]}`);
+    console.log(`The Publisher Is ${myFavGames[gameName[i]].publisher}`);
+    console.log(`The Price Is ${myFavGames[gameName[i]].price}`);
 
-/*
-a: 1
-b: 2
-c: 3
-d: 4
-e: 5
-f: 6
-*/
+    // Check If Nested Object Has Property (bestThree)
+    if (myFavGames[gameName[i]].bestThree) {
+    console.log("- Game Has Releases");
+    console.log(`First => ${myFavGames[gameName[i]].bestThree.one}`);
+    console.log(`Second => ${myFavGames[gameName[i]].bestThree.two}`);
+    console.log(`Third => ${myFavGames[gameName[i]].bestThree.three}`);
+    }
+    console.log("#".repeat(20));
+}
+
+// Ouput
+
+"The Game Name Is Trinity Universe"
+"The Publisher Is NIS America"
+"The Price Is 40"
+"####################"
+"The Game Name Is Titan Quest"
+"The Publisher Is THQ"
+"The Price Is 50"
+"- Game Has Releases"
+"First => Immortal Throne"
+"Second => Ragnarök"
+"Third => Atlantis"
+"####################"
+"The Game Name Is YS"
+"The Publisher Is Falcom"
+"The Price Is 40"
+"- Game Has Releases"
+"First => Oath in Felghana"
+"Second => Ark Of Napishtim"
+"Third => origin"
+"####################"
