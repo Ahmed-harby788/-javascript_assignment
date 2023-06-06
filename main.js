7379eb4978676901fbe3f806728d3cf0adcbb2e6
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-06 21:53:29 +0300

    lessons from 86 to 93 - 1 -

--- main.js
+++ main.js
@@ -1,67 +1,20 @@
-// The Object To Work With
-let myFavGames = {
-    "Trinity Universe": {
-    publisher: "NIS America",
-    price: 40,
-    },
-    "Titan Quest": {
-    publisher: "THQ",
-    bestThree: {
-        one: "Immortal Throne",
-        two: "Ragnarök",
-        three: "Atlantis",
-    },
-    price: 50,
-    },
-    YS: {
-    publisher: "Falcom",
-    bestThree: {
-        one: "Oath in Felghana",
-        two: "Ark Of Napishtim",
-        three: "origin",
-    },
-    price: 40,    },
-};
+console.log(document.querySelector(".element"));
+console.log(document.querySelector("#elzero"));
+console.log(document.querySelector("[name = js]"));
+console.log(document.querySelector("[class = element]"))
 
-  // Code One => How To Get Object Length ?
-let objectLength = Object.keys(myFavGames).length
+console.log(document.querySelectorAll(".element")[0]);
+console.log(document.querySelectorAll("#elzero")[0]);
+console.log(document.querySelectorAll("[name = js]")[0]);
+console.log(document.querySelectorAll("[class = element]")[0])
 
-let gameName = Object.keys(myFavGames)
+console.log(document.getElementsByClassName("element")[0]);
+console.log(document.getElementsByClassName("element")['js']);
+console.log(document.getElementsByClassName("element")['elzero']);
 
-for (let i = 0; i < objectLength; i++) {
-    console.log(`The Game Name Is ${gameName[i]}`);
-    console.log(`The Publisher Is ${myFavGames[gameName[i]].publisher}`);
-    console.log(`The Price Is ${myFavGames[gameName[i]].price}`);
+console.log(document.getElementsByTagName("div")[0])
+console.log(document.getElementsByTagName("div")['js'])
+console.log(document.getElementsByTagName("div")['elzero'])
 
-    // Check If Nested Object Has Property (bestThree)
-    if (myFavGames[gameName[i]].bestThree) {
-    console.log("- Game Has Releases");
-    console.log(`First => ${myFavGames[gameName[i]].bestThree.one}`);
-    console.log(`Second => ${myFavGames[gameName[i]].bestThree.two}`);
-    console.log(`Third => ${myFavGames[gameName[i]].bestThree.three}`);
-    }
-    console.log("#".repeat(20));
-}
-
-// Ouput
-
-"The Game Name Is Trinity Universe"
-"The Publisher Is NIS America"
-"The Price Is 40"
-"####################"
-"The Game Name Is Titan Quest"
-"The Publisher Is THQ"
-"The Price Is 50"
-"- Game Has Releases"
-"First => Immortal Throne"
-"Second => Ragnarök"
-"Third => Atlantis"
-"####################"
-"The Game Name Is YS"
-"The Publisher Is Falcom"
-"The Price Is 40"
-"- Game Has Releases"
-"First => Oath in Felghana"
-"Second => Ark Of Napishtim"
-"Third => origin"
-"####################"
+console.log(document.getElementById("elzero"));
+console.log(document.getElementsByName("js")[0]);
