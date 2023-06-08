6414469344cb3bc0a6c1d19079291f27af5a8f3b
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-08 22:43:18 +0300

    lessons from 86 to 93 - 3 -

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
-for(i = 0; i < 100; i++){
-let myDive = document.createElement("div")
-let myh2 = document.createElement("h2")
-let myp = document.createElement("p")
+let userInput = document.querySelector('[name="dollar"]');
 
-let myHeading= document.createTextNode(`product title ${i + 1}`)
-let myPText = document.createTextNode(`product text ${i + 1}`)
+let result = document.querySelector(".result");
 
-myDive.className = "product"
-myh2.appendChild(myHeading);
-myp.appendChild(myPText);
-myDive.appendChild(myh2);
-myDive.appendChild(myp);
-
-document.body.appendChild(myDive);
+userInput.oninput = function() {
+    let calc =  userInput.value * 15.6;
+    result.innerHTML = `{${userInput.value}} USD Dollar = {${calc.toFixed(2)}} Egyptian Pound`
 }
