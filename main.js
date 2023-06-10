d0fd9e5add9de0a6efc0a885316d046ad22a00d7
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-10 16:38:04 +0300

    lessons from 86 to 93 - 6 -

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let images = document.getElementsByTagName("img");
+let number = document.querySelector("[name = elements]")
+let text = document.querySelector("[name = texts]")
+let selector = document.querySelector("select")
+let optDiv = document.querySelector("select option[value = Div]")
+let optselect = document.querySelector("select option[value = select]")
+let submit = document.querySelector("input [type = submit]")
+let result = document.querySelector(".results")
+result.style.cssText = "display: flex; flex-wrap: wrap;"
+document.forms[0].onsubmit = function (e){
+    result.innerHTML = "";
+    e.preventDefault();
 
-console.log(images)
+    for(i = 0; i < number.value; i++){
+        let newEle = document.createElement(selector.value);
+        newEle.innerHTML = text.value
+        newEle.id = `id-${i + 1}`
+        newEle.style.cssText = "background-color: red; color: white;text-align: center; border-radius: 10px; padding: 10px; margin: 10px;"
+        result.appendChild(newEle)
 
-for(i = 0; i < images.length; i++){
-    if(images[i].hasAttribute("alt")){
-        images[i].alt = "old";
-    }else{
-        images[i].setAttribute("alt", "Elzero New")
     }
+
 }
+
