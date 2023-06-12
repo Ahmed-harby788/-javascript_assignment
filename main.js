5be5aff869a11a03eaf504c90dbcdce195377192
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-12 12:29:38 +0300

    lessons from 94 to 101 - 4 -

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-let myDiv = document.querySelector(".our-element");
-let myP = document.querySelector("p");
-let startDiv = document.createElement("div");
-let endDiv = document.createElement("div");
-let startText = document.createTextNode("Start");
-let endText = document.createTextNode("end");
+let div = document.querySelector("div");
+console.log(div.lastChild.textContent.trim());
 
-startDiv.appendChild(startText);
-endDiv.appendChild(endText);
+// OR
 
-startDiv.className = "start";
-endDiv.className = "end";
-
-startDiv.setAttribute("title", "Start Element");
-endDiv.setAttribute("title", "End Element");
-startDiv.setAttribute("data-value", "Start");
-endDiv.setAttribute("data-value", "End");
-
-myP.remove();
-
-myDiv.before(startDiv)
-myDiv.after(endDiv)
-
+let span = document.querySelector("span");
+console.log(span.nextSibling.nextSibling.nextSibling.textContent.trim());
 
