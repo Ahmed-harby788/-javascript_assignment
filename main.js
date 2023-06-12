c7e2cd4ecef9e6da7f16297c35bf8d234d7d63d9
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-12 12:47:06 +0300

    lessons from 94 to 101 - 5 -

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-let div = document.querySelector("div");
-console.log(div.lastChild.textContent.trim());
-
-// OR
-
-let span = document.querySelector("span");
-console.log(span.nextSibling.nextSibling.nextSibling.textContent.trim());
-
+document.addEventListener("click", function (e){
+    console.log(`This is ${e.target.tagName}`)
+})
