1f447651e268aac693964c909e5313ec4a49c050
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-17 14:16:28 +0300

    lessons from 102 to 110 - 4 -

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 let div = document.createElement("div");
 let cunter = 10;
 
-// div.apped(cunter);
-
 let ruselt = setInterval(function (){
     document.body.appendChild(div);
     div.innerHTML = cunter
     cunter = cunter - 1;
 
-    if(cunter < 0){
-        clearInterval(ruselt); 
+    if(div.innerHTML == 5){
+        open("https://elzero.org/", "", "hight=700, width= 700");
     }
-},1000)
 
-// for (i = 10; i >= 0; i--){
-//     console.log(i)
-// }
+    cunter < 0 ?clearInterval(ruselt) : "";
+    
+},1000)
