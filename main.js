5d8c384e740684554e643407f8bbf1b7a33b579f
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-19 02:20:58 +0300

    lessons from 111 to 114 - 1 -

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-let lis = document.querySelectorAll("ul li");
-let div = document.querySelector(".experiment");
+let selectFont = document.querySelector("#select__font");
+let selectColor = document.querySelector("#select__color");
+let selectFontSize  = document.querySelector("#select__fontSize");
 
-if(window.localStorage.getItem("color")){
-    div.style.backgroundColor = window.localStorage.getItem("color");
-    
-}
+selectFont.addEventListener("change", (e) => {
+    window.localStorage.setItem("font", selectFont.value);
+    document.body.style.fontFamily = localStorage.getItem("font");
+})
+selectColor.addEventListener("change", (e) => {
+    window.localStorage.setItem("color", selectColor.value);
+    document.body.style.color = localStorage.getItem("color");
+})
+selectFontSize.addEventListener("change", (e) => {
+    window.localStorage.setItem("size", selectFontSize.value);
+    document.body.style.fontSize = localStorage.getItem("size");
+})
+document.body.style.fontFamily = localStorage.getItem("font");
+document.body.style.color = localStorage.getItem("color");
+document.body.style.fontSize = localStorage.getItem("size");
 
-lis.forEach((li) => {
-    li.addEventListener("click", (el) => {
-        lis.forEach((li) => {
-            li.classList.remove("active");
-        });
-        window.localStorage.setItem("color", el.target.dataset.color);
-        el.target.classList.add("active");
-        div.style.backgroundColor = window.localStorage.getItem("color");
-    
-    })
-});
+selectFont.value = localStorage.getItem("font");
+selectColor.value = localStorage.getItem("color");
+selectFontSize.value = localStorage.getItem("size");
