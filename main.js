27028f3b093ddf24fd9f265d1526f06545b9ff6a
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-19 21:55:51 +0300

    lessons from 115 to 122 - 5 -

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
-const game = {
-    title: "YS",
-    developer: "Falcom",
-    releases: {"Oath In Felghana": ["USA", "Japan"],"Ark Of Napishtim": {US: "20 USD", JAP: "10 USD",}, Origin: "30 USD"}
-};
-let {title : t, developer : d,} = game;
+let chosen = 3;
 
-let [o,a] = Object.keys(game.releases);
+let myFriends = [
+    { title: "Osama", age: 39, available: true, skills: ["HTML", "CSS"] },
+    { title: "Ahmed", age: 25, available: false, skills: ["Python", "Django"] },
+    { title: "Sayed", age: 33, available: true, skills: ["PHP", "Laravel"] },
+];
 
-let [u,j] = Object.values(game.releases)[0];
+let {title : t, age : a, available : av, skills : [, sk]} = myFriends[chosen - 1];
 
-let {US: u_price, JAP: j_price} = Object.values(game.releases)[1] ;
+if(chosen === 1){
+    console.log(t);
+    console.log(a);
+    console.log(av ? "available" : "Not Available" );
+    console.log(sk);
+}
+if(chosen === 2){
+    console.log(t);
+    console.log(a);
+    console.log(av ? "available" : "Not Available" );
+    console.log(sk);
+}
+if(chosen === 3){
+    console.log(t);
+    console.log(a);
+    console.log(av ? "available" : "Not Available" );
+    console.log(sk);
+}
+// If chosen === 1
 
-let { Origin : or} = game.releases;
+"Osama"
+39
+"Available"
+"CSS"
 
+// If chosen === 2
 
-console.log(`My Favourite Games Style Is ${t} Style`);
-  // My Favourite Games Style Is YS Style
+"Ahmed"
+25
+"Not Available"
+"Django"
 
-console.log(`And I Love ${d} Games`);
-  // And I Love Falcom Games
+// If chosen === 3
 
-console.log(`My Best Release Is ${o} It Released in ${u} & ${j}`);
-  // My Best Release Is Oath In Felghana It Released in USA & Japan
-
-console.log(`Although I Love ${a}`);
-  // Although I Love Ark Of Napishtim
-
-console.log(`${a} Price in USA Is ${u_price}`);
-  // Ark Of Napishtim Price in USA Is 20 USD
-
-console.log(`${a} Price in Japan Is ${j_price}`);
-  // Ark Of Napishtim Price in Japan Is 10 USD
-
-console.log(`Origin Price Is ${or}`);
-  // Origin Price Is 30 USD
+"Sayed"
+33
+"Available"
+"Laravel"
