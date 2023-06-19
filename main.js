bf9c9ad79a22ef33bee76daff0dad78f4ab618f6
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-19 13:13:42 +0300

    lessons from 115 to 122 - 1 -

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
-let email = document.querySelector("#email");
-let text = document.querySelector("#text");
-let password = document.querySelector("#password");
-let select_value = document.querySelector("#select_value")
+let myNumbers = [1, 2, 3, 4, 5];
 
-email.addEventListener("input", (el) => {
-    localStorage.setItem("email", email.value);
-})
-text.addEventListener("input", (el) => {
-    localStorage.setItem("text", text.value);
-})
-password.addEventListener("input", (el) => {
-    localStorage.setItem("password", password.value);
-})
-select_value.addEventListener("input", (el) => {
-    localStorage.setItem("select_value", select_value.value);
-})
+let [a, , , ,e] = myNumbers
 
-email.value = sessionStorage.getItem("email");
-text.value = sessionStorage.getItem("text");
-password.value = sessionStorage.getItem("password");
-select_value = sessionStorage.getItem("select_value")
+console.log(a * e); // 5
