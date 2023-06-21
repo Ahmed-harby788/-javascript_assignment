cf47ba3ead8abfb737d6bb8717e2a51af2b9b719
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-06-21 22:07:01 +0300

    lessons from 123 to 133 - 4 -

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-let myInfo = {
-    username: "Osama",
-    role: "Admin",
-    country: "Egypt",
-};
+let theNumber = 100020003000;
 
-let myMap = new Map(Object.entries(myInfo));
-// let myMap = new Map([
-//     ["username" , "Osama"],
-//     ["role" , "Admin"],
-//     ["country" , "Egypt"]
-// ]);
-
-console.log(myMap);
-console.log(myMap.size);
-console.log(myMap.has("role"));
-
-
-// Needed Output
-// Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
-// 3
-// true\
-
-
+console.log(+[Array.from(new Set(theNumber.toString())).sort().splice(+[]).join("")]);
