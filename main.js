5bdd2a1a8aa6ef1aa0cbf9a2bd31a20299c6b4a5
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-07-06 02:30:12 +0300

    lessons from 134 to 146 - 2 -

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let ip = "2001:db8:3333:4444:5555:6666:7777:8888";
 
-let re = ip.match(/\d{4}:\w{3}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}/gi);
+let specialNames = "Os10O OsO Os100O Osa100O Os1000 Os100m";
+console.log(specialNames.match(/os\d*o/ig));
 
-console.log(re);
-
+// Output
+// ['Os10O', 'OsO', 'Os100O']
