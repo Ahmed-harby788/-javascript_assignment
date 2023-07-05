d2a53295e2b5dfee3f57c3c048ecfd5d0756b341
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-07-06 02:28:05 +0300

    lessons from 134 to 146 - 1 -

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-let theNumber = 100020003000;
+let ip = "2001:db8:3333:4444:5555:6666:7777:8888";
 
-console.log(+[Array.from(new Set(theNumber.toString())).sort().splice(+[]).join("")]);
+let re = ip.match(/\d{4}:\w{3}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}/gi);
+
+console.log(re);
+
