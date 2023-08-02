6ae05bb532e08a6dd8583d664cdf39c58c51b46f
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-08-02 20:22:42 +0300

    lessons from 147 to 158 - 1 -

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-let url1 = 'elzero.org';
-let url2 = 'http://elzero.org';
-let url3 = 'https://elzero.org';
-let url4 = 'https://www.elzero.org';
-let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
 
-let re = /(https*:\/\/)?(\w+.)?\w+.org(:8080\/articles.php\?id=100&cat=topics)?/g; // Write Your Pattern Here
+class Car{
+    constructor(name, model, price){
+        this.name = name
+        this.model = model
+        this.price = price
+    }
+    run() {
+        return `Running Now`
+    }
+    Stop() {
+        return `Car is Stopped`
+    }
+}
 
-console.log(url1.match(re));
-console.log(url2.match(re));
-console.log(url3.match(re));
-console.log(url4.match(re));
-console.log(url5.match(re));
+let Car1 = new Car("MG", "2022", 420000);
+let Car2 = new Car("car2", "2021", 20000);
+let Car1ar3 = new Car("car3", "2022", 15000);
+
+console.log(`Car One Name Is ${Car1.name} And Model Is ${Car1.model} And Price Is ${Car1.price}`);
+console.log(`Car Is ${Car1.run()}`);
+
+// Needed Output
+
+"Car One Name Is MG And Model Is 2022 And Price Is 420000"
+"Car Is Running Now"
