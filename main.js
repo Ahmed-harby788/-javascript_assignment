e3bae79343e7669117f3ac3cfe481dcfd6643ed7
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-08-03 12:43:18 +0300

    lessons from 147 to 158 - 3 -

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
-class Phone {
-    constructor(name, serial, price) {
-    this.name = name;
-    this.serial = serial;
-    this.price = price;
+// Edit The Class
+class User {
+    #c;
+constructor(username, card) {
+    this.u = username;
+    this.#c = card;
+    }
+    getCard(){
+        return this.#c.toString().match(/\d{4}/g).join("-");
+    }
+    showData(){
+        return `Hello ${this.u} Your Credit Card Number Is ${this.getCard()}`
     }
 }
 
-  // Write Tablet Class Here
-class Tablet extends Phone{
-    constructor(name, serial, price, size) {
-        super(name, serial, price);
-        this.size = size || "Unknown";
-    }
-    fullDetails() {
-        return `${this.name} serial is ${this.serial} and Size is ${this.size}`;
-    }
-}
+  // Do Not Edit Anything Below
 
-let TabletOne = new Tablet("iPad", 100200300, 1500, 12.9);
-let TabletTwo = new Tablet("Nokia", 350450650, 800, 10.5);
-let TabletThree = new Tablet("LG", 250450650, 650);
+let userOne = new User("Osama", "1234-5678-1234-5678");
+let userTwo = new User("Ahmed", "1234567812345678");
+let userThree = new User("Ghareeb", 1234567812345678);
 
-console.log(`${TabletOne.fullDetails()}`);
-  // iPad Serial is 100200300 And Size Is 12.9
-console.log(`${TabletTwo.fullDetails()}`);
-  // Nokia Serial is 350450650 And Size Is 10.5
-console.log(`${TabletThree.fullDetails()}`);
-  // LG Serial is 250450650 And Size Is Unknown
+
+console.log(userOne.showData());
+  // Hello Osama Your Credit Card Number Is 1234-5678-1234-5678
+
+console.log(userTwo.showData());
+  // Hello Ahmed Your Credit Card Number Is 1234-5678-1234-5678
+
+console.log(userThree.showData());
+  // Hello Ghareeb Your Credit Card Number Is 1234-5678-1234-5678
+
+  console.log(userOne.c); // Prevent Accessing To Card Property Here
+  // Undefined
