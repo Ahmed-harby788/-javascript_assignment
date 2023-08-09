1c362357b3137925cb68afed8bc19def7611c752
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-08-10 00:56:17 +0300

    lessons from 159 to 168 - 7 -

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-// Write Your Generator Function Here
-
-function* gen(){
-  let i = 14;
-  let num = 140;
-  let sum = i + num;
-  yield i;
-  while (true){
-    yield sum;
-    sum += num += 200;
-  }
+function* genNumbers() {
+  yield* [1, 2, 2, 2, 3, 4, 5];
+}
+function* genLetters() {
+  yield* ["A", "B", "B", "B", "C", "D"];
 }
 
+// Write Your Generator Function Here
+function* genAll(){
+  yield* new Set(genNumbers());
+  yield* new Set(genLetters());
+}
 
-let generator = gen();
+let generator = genAll();
 
-console.log(generator.next()); // {value: 14, done: false}
-console.log(generator.next()); // {value: 154, done: false}
-console.log(generator.next()); // {value: 494, done: false}
-console.log(generator.next()); // {value: 1034, done: false}
-console.log(generator.next()); // {value: 1774, done: false}
-console.log(generator.next()); // {value: 2714, done: false}
-console.log(generator.next()); // {value: 3854, done: false}
-console.log(generator.next()); // {value: 5194, done: false}
-console.log(generator.next()); // {value: 6734, done: false}
+console.log(generator.next()); // {value: 1, done: false}
+console.log(generator.next()); // {value: 2, done: false}
+console.log(generator.next()); // {value: 3, done: false}
+console.log(generator.next()); // {value: 4, done: false}
+console.log(generator.next()); // {value: 5, done: false}
+console.log(generator.next()); // {value: "A", done: false}
+console.log(generator.next()); // {value: "B", done: false}
+console.log(generator.next()); // {value: "C", done: false}
+console.log(generator.next()); // {value: "D", done: false}
