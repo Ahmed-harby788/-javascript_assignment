94dfe8ce90ba8735c45e9bcbda01e8e259e50a01
ahmedharpy788 <ahmedharpy788@gmail.com>
2023-08-14 01:17:44 +0300

    lessons from 169 to 178 - 3 -

--- main.js
+++ main.js
@@ -1,38 +1,17 @@
-// let MyReq = new XMLHttpRequest();
-// MyReq.open("Get", "https://api.github.com/users/Ahmed-harby788");
-// MyReq.send();
-// // console.log(MyReq);
-
-//   MyReq.onreadystatechange = function () {
-//     if(this.readyState === 4 && this.status === 200){
-//       console.log(this.responseText)
-    
-//     let jsMyReq = JSON.parse(this.responseText);
-//     console.log(jsMyReq);
-
-//     for(let i = 0; i < jsMyReq.length; i++){
-//       console.log(jsMyReq[i])
-//       let div = document.createElement("div")
-//       let reboName = document.createTextNode(jsMyReq)
-//       div.appendChild(reboName)
-//       document.body.appendChild(div);
-//     }
-//   }
-// }
-
-
-
 let MyReq = new XMLHttpRequest();
 MyReq.open("Get","articles.json");
 MyReq.send();
 
 MyReq.onreadystatechange = function () {
   if(this.readyState === 4 && this.status === 200){
-    console.log(this.responseText)
-    setTimeout(() => {
-      console.log("Data Loaded")
-    }, 0);
+    let mainData = JSON.parse(this.responseText);
+    for(let i = 0; i < mainData.length; i++){
+      mainData[i].category = "All"
+    }
+    console.log(mainData);
+    console.log(typeof mainData);
   }
+  let updatedData = JSON.stringify(mainData);
+  console.log(updatedData);
 }
 
-// console.log(MyReq);
